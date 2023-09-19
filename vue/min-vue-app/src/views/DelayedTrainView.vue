f77febb9a91ed359dd09cbde8fe0e37a01cc5706
elan19 <elan19@student.bth.se>
2023-09-19 16:58:40 +0200

    Lagt till map component samt markörer på kartan för varje försenat tåg

--- vue/min-vue-app/src/views/DelayedTrainView.vue
+++ vue/min-vue-app/src/views/DelayedTrainView.vue
@@ -1,14 +1,35 @@
 <template>
     <h1>Delayed Trains</h1>
+    <div class="container" id="container">
+        <div id="delayed-trains" class="delayed-trains">
+            <h1>Alla försenade tåg</h1>
+            <p>Försenade tåg - Objekt</p>
+        </div>
+        <Map></Map>
+    </div>
 </template>
 
 <script>
+import Map from '../components/Map.vue';
 
 export default {
     name: "DelayedView",
+    components: {
+        Map
+    },
     props: {}
 }
 
 </script>
 
-<style></style>
+<style>
+    .container {
+        display: flex;
+    }
+    .delayed-trains {
+        width: 50%;
+        height: 100%;
+        margin: 0;
+        padding: 0;
+    }
+</style>
