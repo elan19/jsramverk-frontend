e7233329dbcb6bf3681d3667a5c05d42b2ab7334
Albin Stegler <albin.stegler@gmail.com>
2023-09-19 13:48:44 +0200

    created base for vue app

---
+++ vue/min-vue-app/src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav>
+        <ul>
+            <li><router-link to="/">Me</router-link></li>
+            <li><router-link to="/CreateTicket">Tåg</router-link></li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'NavBar',
+    props: {}
+}
+</script>
---
+++ vue/min-vue-app/src/views/CreateTicketView.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>CreateTicket</h1>
+    <CreateTicket />
+</template>
+
+<script>
+import CreateTicket from '../components/CreateTicket'
+
+export default {
+
+    name: "DelayedView",
+    props: {},
+    components: {
+        CreateTicket,
+    }
+}
+
+</script>
+
+<style></style>
