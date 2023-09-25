5748dd7cfa1c0384fea106c22bc1066925b00116
Albin Stegler <albin.stegler@gmail.com>
2023-09-25 10:02:52 +0200

    added support for local and dev settings

--- vue/min-vue-app/src/main.js
+++ vue/min-vue-app/src/main.js
@@ -3,8 +3,8 @@ import App from './App.vue';
 import router from '@/router/index.js';
 import 'leaflet/dist/leaflet.css';
 
-const app = createApp(App);
 
+const app = createApp(App);
 app.use(router);
 
 app.mount('#app');
